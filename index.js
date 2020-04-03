b9535d7d81a84b2ff2bc5564a2fa99333a95140a
vytautassugintas <vytautas.sugintas@gmail.com>
2020-04-03 17:03:26 +0300

    fix: initialize as null

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var get = require('lodash.get');
-var serverVars = {};
+const get = require('lodash.get');
+
+let serverVars = null;
 
 // on the client, this module exports the bootstrapped serverVars
 if (typeof window !== 'undefined' && window.__SERVER_VARS__) {
     serverVars = window.__SERVER_VARS__;
-    serverVars.get = function(key) {
+    serverVars.get = function (key) {
         return get(serverVars, key);
     };
 }
